4822f5e8b499b6a6f44a958dea9fba1432769c26
annakobets <79173341+annakobets@users.noreply.github.com>
2021-03-06 05:38:35 +0900

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,9 +1,9 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    int a,b;
-    while (a!=b)
-        if (a>b) a-=b;
-    else b-=a;
+    int a, b;
+    while (a != b)
+        if (a > b) a -= b;
+    else b -= a;
     return a;
 }
